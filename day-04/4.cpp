31256e34289ce77e7097d65967e27825020b5a8e
r-clifford <r-clifford@example.com>
2023-12-04 13:32:09 -0500

    Day 4 part 2

--- day-04/4.cpp
+++ day-04/4.cpp
@@ -1,10 +1,11 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <unordered_set>
 #include <vector>
 
-int parseLine(std::string line) {
+int parseLine1(std::string line) {
   std::string::iterator it = line.begin();
   while (*it != ':') {
     it++;
@@ -57,20 +58,86 @@ int parseLine(std::string line) {
   }
   return result;
 }
+int parseLine2(std::string line) {
+  std::string::iterator it = line.begin();
+  while (*it != ':') {
+    it++;
+  }
+  it += 2;
+  std::unordered_set<int> winningSet;
+  std::string numStr;
+  while (*it != '|') {
+    if (*it == ' ') {
+      int tmp;
+      try {
+        tmp = std::stoi(numStr);
+      } catch (std::invalid_argument _) {
+        it++;
+        continue;
+      }
+      winningSet.insert(tmp);
+      numStr.clear();
+    }
+    if (std::isdigit(*it)) numStr += *(it);
+    it++;
+  }
+  it += 2;
+  numStr.clear();
+  std::unordered_set<int> numbers;
+  while (it != line.end()) {
+    if (*it == ' ') {
+      int tmp;
+      try {
+        tmp = std::stoi(numStr);
+      } catch (std::invalid_argument _) {
+        it++;
+        continue;
+      }
+      numbers.insert(tmp);
+      numStr.clear();
+    }
+    if (std::isdigit(*it)) numStr += *(it);
+    if (it == line.end()) {
+      break;
+    } else {
+      it++;
+    }
+  }
+  int matches = 0;
+  for (auto &p : numbers) {
+    if (winningSet.find(p) != winningSet.end()) {
+      matches++;
+    }
+  }
+  return matches;
+}
 int main() {
   std::ifstream input("4.txt");
   std::vector<std::string> lines;
   std::string tmp;
+  std::vector<int> counts = {};
   while (std::getline(input, tmp)) {
     tmp += ' ';
 
     lines.push_back(tmp);
-    std::cout << tmp << std::endl;
     tmp.clear();
+    counts.push_back(1);
   }
   int total = 0;
+  size_t i = 0;
   for (auto line : lines) {
-    total += parseLine(line);
+    int reward;
+    if (counts[i] > 0) {
+      reward = parseLine2(line);
+    }
+    while (counts[i]-- > 0) {
+      for (size_t j = i + 1; j <= i + reward; j++) {
+        counts[j]++;
+      }
+
+      total++;
+    }
+    i++;
   }
   std::cout << total << std::endl;
 }
